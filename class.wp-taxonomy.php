9fa258edaf0025407480f3c6add5a3af1ab6ff2b
mdawaffe <mdawaffe@9041e203-7010-4fe5-93e1-c5c2efc1e134>
2008-05-19 19:38:20 +0000

    Make sure taxonomy args are set before using. Props Sam_a.  [WP7952]

--- class.wp-taxonomy.php
+++ class.wp-taxonomy.php
@@ -1008,14 +1008,14 @@ class WP_Taxonomy { // [WP7919]
 		if ( count($taxonomies) > 1 ) {
 			foreach ( $taxonomies as $index => $taxonomy ) {
 				$t = $this->get_taxonomy($taxonomy);
-				if ( is_array($t->args) && $args != array_merge($args, $t->args) ) {
+				if ( isset($t->args) && is_array($t->args) && $args != array_merge($args, $t->args) ) {
 					unset($taxonomies[$index]);
 					$terms = array_merge($terms, $this->get_object_terms($object_ids, $taxonomy, array_merge($args, $t->args)));
 				}
 			}
 		} else {
 			$t = $this->get_taxonomy($taxonomies[0]);
-			if ( is_array($t->args) )
+			if ( isset($t->args) && is_array($t->args) )
 				$args = array_merge($args, $t->args);
 		}
 
