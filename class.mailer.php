2dc675fd0c299057037c373fd6afa6adc8dd3c4e
sambauers <sambauers@9041e203-7010-4fe5-93e1-c5c2efc1e134>
2008-10-08 05:04:48 +0000

    Incorrect path to smtp mailer class in mailer class.

--- class.mailer.php
+++ class.mailer.php
@@ -509,7 +509,7 @@ class PHPMailer {
    * @return bool
    */
   function SmtpSend($header, $body) {
-    include_once($this->PluginDir . 'class-smtp.php');
+    include_once('class.mailer-smtp.php');
     $error = '';
     $bad_rcpt = array();
 
