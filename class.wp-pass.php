e38c40b894eab9d5f24b6514f9bfd5e2e760856f
sambauers <sambauers@9041e203-7010-4fe5-93e1-c5c2efc1e134>
2009-06-10 14:43:29 +0000

    Use transient functions in WP_Pass::rand()

--- class.wp-pass.php
+++ class.wp-pass.php
@@ -111,7 +111,7 @@ class WP_Pass {
 	function rand( $min = 0, $max = 0 ) {
 		global $rnd_value;
 
-		$seed = backpress_get_option('seed');
+		$seed = backpress_get_transient('random_seed');
 
 		// Reset $rnd_value after 14 uses
 		// 32(md5) + 40(sha1) + 40(sha1) / 8 = 14 random numbers from $rnd_value
@@ -120,7 +120,7 @@ class WP_Pass {
 			$rnd_value .= sha1($rnd_value);
 			$rnd_value .= sha1($rnd_value . $seed);
 			$seed = md5($seed . $rnd_value);
-			backpress_update_option('seed', $seed);
+			backpress_set_transient('random_seed', $seed);
 		}
 
 		// Take the first 8 digits for our value
