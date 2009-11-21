1b253736e5ef85747c47a2c7c8117df446f96a67
westi <westi@9041e203-7010-4fe5-93e1-c5c2efc1e134>
2009-11-21 09:17:45 +0000

    Ensure that make_clickable links correctly when the link is placed in brackets and has trailing punctuation. Fixes WP#11211 props filosofo.

--- functions.formatting.php
+++ functions.formatting.php
@@ -1,5 +1,5 @@
 <?php
-// Last sync [WP12088]
+// Last sync [WP12250]
 
 /**
  * From WP wp-includes/formatting.php
@@ -1063,7 +1063,7 @@ if ( !function_exists( 'make_clickable' ) ) :
 function make_clickable($ret) {
 	$ret = ' ' . $ret;
 	// in testing, using arrays here was found to be faster
-	$ret = preg_replace_callback('#(?<=[\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#$%&~/=?@\[\](+-]|[.,;:](?![\s<]|(\))?([\s]|$))|(?(1)\)(?![\s<]|$)|\)))+)#is', '_make_url_clickable_cb', $ret);
+	$ret = preg_replace_callback('#(?<=[\s>])(\()?([\w]+?://(?:[\w\\x80-\\xff\#$%&~/=?@\[\](+-]|[.,;:](?![\s<]|(\))?([\s]|$))|(?(1)\)(?![\s<.,;:]|$)|\)))+)#is', '_make_url_clickable_cb', $ret);
 	$ret = preg_replace_callback('#([\s>])((www|ftp)\.[\w\\x80-\\xff\#$%&~/.\-;:=,?@\[\]+]+)#is', '_make_web_ftp_clickable_cb', $ret);
 	$ret = preg_replace_callback('#([\s>])([.0-9a-z_+-]+)@(([0-9a-z-]+\.)+[0-9a-z]{2,})#i', '_make_email_clickable_cb', $ret);
 	// this one is not in an array because we need it to run last, for cleanup of accidental links within links
