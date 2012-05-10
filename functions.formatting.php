ce1ad3b36caceae5bb80fa5059e3d53da9877104
nacin <nacin@9041e203-7010-4fe5-93e1-c5c2efc1e134>
2012-05-10 19:42:50 +0000

    Add esc_textarea() to functions.formatting.php.

--- functions.formatting.php
+++ functions.formatting.php
@@ -1757,6 +1757,21 @@ function esc_attr( $text ) {
 }
 endif;
 
+if ( !function_exists( 'esc_textarea' ) ) :
+/**
+ * Escaping for textarea values.
+ *
+ * @since 3.1
+ *
+ * @param string $text
+ * @return string
+ */
+function esc_textarea( $text ) {
+	$safe_text = htmlspecialchars( $text, ENT_QUOTES );
+	return apply_filters( 'esc_textarea', $safe_text, $text );
+}
+endif;
+
 if ( !function_exists( 'attribute_escape' ) ) :
 /**
  * Escaping for HTML attributes.
